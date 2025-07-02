8931b3297d7e4a2d0c5d17ae7a2a51b425cc1a66
galleb <vikeevsv@gmail.com>
2025-07-02 21:31:52 +0300

    Modification for Macbook 12 inch 2017

---
+++ README.md
@@ -0,0 +1,29 @@
+Kbdlight
+========
+
+kbdlight is very simple application that changes MacBook 12 inch 2007 (Macbook10.1) keyboard
+backlight level.
+
+While usable standalone, the main goal is for it to be used by other
+applications, including things like sxhkd or xbindkeys.
+
+Usage
+-----
+Usage is quite simple:
+
+    kbdlight up [<percentage>]|down [<percentage>]|off|max|get|getp|set <value>
+
+`getp` returns the percentage of brightness for the value.
+`percentage` should be an integer between 0 and 100.
+`value` should be an integer between 0 the maximum brightness value (usually
+255, but this *may* vary).
+
+Project status
+--------------
+
+This project is finshed. It works, and hasn't had any issues in years. Since I
+no longer have a MacBook, I can no longer accurately test this either. If it's
+broken, you're welcome to maintain this package.
+
+Copyright (c) 2013-2022, Hugo Osvaldo Barrera <hugo@whynothugo.nl> and
+contributors. See LICENCE for details.
---
+++ kbdlight.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define BLPATH		"/sys/class/leds/spi::kbd_backlight"
+#define BLVALFILE	BLPATH "/brightness"
+#define BLMAXFILE	BLPATH "/max_brightness"
+#define BLCHANGE	16
+
+void usage() {
+    printf("Usage: kbdlight [up [<percentage>]|down [<percentage>]|off|max|get|getp|set <value>]\n");
+    exit(0);
+}
+
+int main(int argc, char* argv[]) {
+  int current, change, next, maxval;
+  FILE *file;
+
+  if (argc < 2) {
+    usage();
+  }
+
+  if(!(file = fopen(BLVALFILE, "r"))) {
+    perror("failed to open the keyboard backlight file: " BLVALFILE);
+    return 1;
+  }
+  fscanf(file, "%d", &current);
+  fclose(file);
+
+  if((file = fopen(BLMAXFILE, "r"))) {
+    fscanf(file, "%d", &maxval);
+    fclose(file);
+  } else {
+    maxval = 255;
+  }
+
+  if (!strcmp(argv[1], "up") || !strcmp(argv[1], "down")) {
+    if (argv[2]) {
+      char *endp;
+
+      change = strtol(argv[2], &endp, 10);
+
+      if (endp == argv[2]) {
+        fprintf(stderr, "percentage must be a number from 0 to 100.\n");
+        return 1;
+      }
+
+      change = change * 0.01 * maxval;
+    } else {
+      change = BLCHANGE;
+    }
+  }
+
+  if (!strcmp(argv[1], "up"))
+    next = current >= maxval - change ? maxval : current + change;
+  else if (!strcmp(argv[1], "down"))
+    next = current <= change ? 0 : current - change;
+  else if (!strcmp(argv[1], "off"))
+    next = 0;
+  else if (!strcmp(argv[1], "max"))
+    next = maxval;
+  else if (!strcmp(argv[1], "set")) {
+    char *endp;
+    if(!argv[2] || (next = strtol(argv[2], &endp, 10), endp == argv[2])) {
+      fprintf(stderr, "set must be followed by a number from 0 to %d\n", maxval);
+      return 1;
+    }
+  } else if(!strcmp(argv[1], "get")) {
+    printf("%d\n", current);
+    return 0;
+  } else if(!strcmp(argv[1], "getp")) {
+    float percentage = current * 100 / 255;
+    printf("%.f\n", percentage);
+    return 0;
+  } else
+    usage();
+
+  if((file = fopen(BLVALFILE, "w"))) {
+    fprintf(file, "%d", next);
+    fclose(file);
+  } else {
+    perror("failed to write to the keyboard backlight file: " BLVALFILE);
+  }
+  return 0;
+}
